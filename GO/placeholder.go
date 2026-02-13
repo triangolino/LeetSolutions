2913abfb89e5a7a37a4f4db27551b4ece8e86ef4
Triangolino <matteo.piccioni@outlook.it>
2026-02-13 19:52:35 +0100

    just checking changes

--- GO/placeholder.go
+++ GO/placeholder.go
@@ -2,7 +2,7 @@ package main
 
 import "fmt"
 
-// Starter function for every leetCode problem
+// Starter function for every LeetCode problem
 func main() {
 	//space for func
 	fmt.Println("executed")
